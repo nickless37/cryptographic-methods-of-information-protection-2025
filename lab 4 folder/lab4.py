c62386f69d6cd7de753ff4cc55cf4e04c2df09d0
Danua <137786976+nickless37@users.noreply.github.com>
2025-10-13 19:01:37 +0300

    task 2 done

--- lab 4 folder/lab4.py
+++ lab 4 folder/lab4.py
@@ -1,4 +1,5 @@
 from Crypto.Cipher import ARC4  #ARC4 - алгоритм RC4 без авторського права та бренду
+from Crypto.Cipher import Salsa20
 
 Email = "dtimokhin"
 text = Email.encode('utf-8') 
@@ -9,12 +10,27 @@ def TaskOne():
     key = bytes.fromhex("0123456789ABCDEF0123456789ABCDEF") # unhexlify має той же ефект, що і bytes.fromhex
     CipherType = ARC4.new(key)
     cipherText = CipherType.encrypt(text)
-    print("result:", cipherText.hex())
+    print("result(hex):", cipherText.hex())
 
     # перевірка
     # decipher = ARC4.new(key)   #Для розшифрування треба створювати новий об'єкт, бо у цьому шифрі об'єкт шифру змінюється при використанні
     # decrypted = decipher.decrypt(cipherText)
     # print("Decrypted:", decrypted.decode())
 
+def TaskTwo():
+    keydata = bytes.fromhex("A1B2C3D4E5F60718293A4B5C6D7E8F901112131415161718192A2B2C2D2E2F30")
+    CipherType = Salsa20.new(key=keydata)
+    noncedata = CipherType.nonce #nonce- 8 байт, які необхідні для розшифрування 
+    cipherText = CipherType.encrypt(text)
+    print("result(hex): ",cipherText.hex())
+    print("Nonce (hex):", noncedata.hex())
+
+    #checkup
+    # decipher = Salsa20.new(key=keydata, nonce=noncedata)
+    # decrypted = decipher.decrypt(cipherText)
+    # print("Decrypted:", decrypted.decode()) 
+
 print("Task 1")
 TaskOne()
+print("Task 2")
+TaskTwo()
